a2326371c83fef0730a226ce7a5cd71caa01fc52
Maarten Isaacson <89573725+M-Isaacson@users.noreply.github.com>
2021-11-09 18:32:31 +0100

    Delete tempCodeRunnerFile.py

--- code/arguments/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-    # Part 1: Greet Template
-    # print(f"Part 1: Greet Template\n{greet('Bob')}")
-
-    # Part 2: Force
-    # print(f"Part 2: Force\n{force(3.0,'neptune')}")
-
-    # Part 3: Gravity
-    # print(f"Part 3: Gravity\n{pull(0.1,(5.972*(10**24)),6.371*(10**6))}")
-
